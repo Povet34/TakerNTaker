eb18f3cd910e250bbae1687021b0120a3b320c0d
wotkd34 <wotkd34@naver.com>
2024-08-29 23:21:49 +0900

    레이어 분할

--- TakerNTaker/Assets/_Scripts/Commons/Definitions.cs
+++ TakerNTaker/Assets/_Scripts/Commons/Definitions.cs
@@ -1,5 +1,17 @@
+using UnityEngine;
+
 public static class Definitions
 {
+
+    #region Layer
+
+    public static LayerMask PLAYER_LAYER = LayerMask.GetMask("Player");
+    public static LayerMask OBJECT_LAYER = LayerMask.GetMask("Object");
+    public static LayerMask BEHIND_LAYER = LayerMask.GetMask("BehindMask");
+    public static LayerMask DAMAGABLE_LAYER = PLAYER_LAYER | BEHIND_LAYER;
+
+    #endregion
+
     public enum eClassType
     {
         None,
